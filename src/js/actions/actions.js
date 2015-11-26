a36cb71c6b598f52db232e1b2d0e42ae333f7db4
xushanchuan <aisensiy@163.com>
2015-11-26 16:06:22 +0800

    action for login

--- src/js/actions/actions.js
+++ src/js/actions/actions.js
@@ -1,6 +1,4 @@
-/*
- * action types
- */
+import request from 'superagent';
 
 export const ADD_TODO = 'ADD_TODO';
 export const COMPLETE_TODO = 'COMPLETE_TODO';
@@ -32,4 +30,56 @@ export function setVisibilityFilter(filter) {
   return { type: SET_VISIBILITY_FILTER, filter }
 }
 
-export const 
+//function getRequestActions(name) {
+//  function getAction(name, suffix) {
+//    return `${name.toUpperCase()}_${suffix.toUpperCase()}`;
+//  }
+//
+//  return ({
+//    [getAction(name, "REQUEST")]: getAction(name, "REQUEST"),
+//    [getAction(name, "SUCCESS")]: getAction(name, "SUCCESS"),
+//    [getAction(name, "FAILURE")]: getAction(name, "FAILURE")
+//  });
+//}
+
+export const LOGIN_REQUEST = 'LOGIN_REQUEST';
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGIN_FAILURE = 'LOGIN_FAILURE';
+
+const API_PREFIX = 'http://localhost:3000';
+
+export function loginSuccess(entity) {
+  return {
+    type: LOGIN_SUCCESS,
+    payload: entity
+  };
+}
+
+export function loginFailure() {
+  return {
+    type: LOGIN_FAILURE
+  }
+}
+
+export function loginRequest() {
+  return {
+    type: LOGIN_REQUEST
+  }
+}
+
+export function login(entity) {
+  return (dispatch) => {
+    dispatch(loginRequest());
+    request
+        .post(`${API_PREFIX}/members/login`)
+        .send({name: entity.name, password: entity.password})
+        .end((err, res) => {
+          if (res.type != 2) {
+            dispatch(loginFailure());
+          } else {
+            dispatch(loginSuccess(entity));
+          }
+        })
+  }
+}
+
